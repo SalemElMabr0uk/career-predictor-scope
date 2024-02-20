2b92e6b71a5d24ea24767f66f0ecc3abfbbea431
jaweher1925 <jaweherhichri07@gmail.com>
2024-02-20 14:17:28 +0100

    v1 global css

--- pages/Login/index.tsx
+++ pages/Login/index.tsx
@@ -1,11 +1,10 @@
 import Link from 'next/link';
 import React from 'react';
+import "../../app/globals.css"
 
 export default function Login() {
- 
-
   return (
- 
+    
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="bg-[#212121] p-8 rounded shadow-md w-96">
           <h1 className="text-4xl text-center font-semibold mb-8">Login</h1>
